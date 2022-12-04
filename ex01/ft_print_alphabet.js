7b1205a77969cd71609bc70d76d553bc37e80e57
Emma Rouquet <emma.rouquet7@gmail.com>
2022-12-04 11:46:39 +0100

    ex01

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,6 @@
+function alphabet(){
+  //mieux vaut juste faire console.log("abcdefghijklmnopqrstuvwxyz")
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  console.log(alphabet.join(''))
+}
+alphabet();
