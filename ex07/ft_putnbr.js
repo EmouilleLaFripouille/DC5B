7a7867125183abf7b07e193e24b2cd233626ba19
Emma Rouquet <emma.rouquet7@gmail.com>
2022-12-04 12:32:31 +0100

    ex07

---
+++ ex07/ft_putnbr.js
@@ -0,0 +1,22 @@
+function combination(number) {
+  let result = [];
+  let numberStr = number.toString();
+  let numberLength = numberStr.length;
+  
+  function permutate(arr, m = []) {
+    if (arr.length === 0) {
+      result.push(m.join(''))
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permutate(curr.slice(), m.concat(next))
+     }
+   }
+  }
+  permutate(numberStr.split(''));
+  document.write('Nombre en parametre : '+number);
+  document.write('<br>');
+  document.write('Combinaisons possibles : '+result);
+}
+combination(156);
